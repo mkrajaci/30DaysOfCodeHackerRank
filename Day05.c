3103273a9f9435d8b5b6493ffe391122cb3257f6
Mario Krajacic <krajacic.mario@gmail.com>
2017-08-21 14:01:54 +0200

    Update Day05.c

--- Day05.c
+++ Day05.c
@@ -6,6 +6,8 @@
 #include <limits.h>
 #include <stdbool.h>
 
+// https://www.hackerrank.com/challenges/30-loops
+
 /*Task 
 Given an integer, n, print its first 10 multiples. Each multiple n x i (where 1 <= i <= 10) should be printed on a new line in the form: n x i = result.
 
